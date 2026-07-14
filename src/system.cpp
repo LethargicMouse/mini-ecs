f8ad4d6da33e1dacca34447aaae8cae241e1acc6
George Kozirev <gkozirev.0@gmail.com>
2026-07-15 01:24:14 +0300

    reset

--- src/system.cpp
+++
@@ -1,7 +0,0 @@
-#include "system.h"
-
-void gravitySystem(float dt, TransformComponent *transformComponent,
-                   SpeedComponent *speedComponent) {
-  speedComponent->speed.y += 10 * dt;
-  transformComponent->pos += speedComponent->speed * dt;
-}
--- src/system.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SYSTEM_H_
-#define _SYSTEM_H_
-
-#include "component.h"
-
-void gravitySystem(float dt, TransformComponent *transformComponent,
-                   SpeedComponent *speedComponent);
-
-#endif
