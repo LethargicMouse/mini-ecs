4fafac655ba042811860e26c752f839b20eac96d
George Kozirev <gkozirev.0@gmail.com>
2026-07-18 18:47:41 +0300

    comment math.h

--- src/math.h
+++ src/math.h
@@ -1,11 +1,14 @@
 #ifndef MATH_H
 #define MATH_H
 
+/// A 3D vector class.
 template <typename T> struct Vec3 {
   T x, y, z;
 
+  /// Multiply by a constant.
   Vec3 operator*(float k) const { return Vec3(x * k, y * k, z * k); }
 
+  /// Add two vectors.
   void operator+=(Vec3 other) {
     x += other.x;
     y += other.y;
@@ -13,6 +16,7 @@ template <typename T> struct Vec3 {
   }
 };
 
+/// A 2D vector class for `rotation`.
 template <typename T> struct Vec2 {
   T x, y;
 };
