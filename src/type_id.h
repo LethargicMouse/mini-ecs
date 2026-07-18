eceac3e98887a3cbe651ccb491eb43d074b50f09
George Kozirev <gkozirev.0@gmail.com>
2026-07-18 18:29:11 +0300

    comment type_id.h

--- src/type_id.h
+++ src/type_id.h
@@ -8,11 +8,14 @@
 
 using TypeID = ID;
 
+/// Return the value of the static counter and increase it.
 inline TypeID nextTypeID() {
   static TypeID next = 0;
   return next++;
 }
 
+/// Gets a unique value of the counter once for every type.
+/// Only stable if this header is included exactly once.
 template <typename T> TypeID getTypeID() {
   static const TypeID id = nextTypeID();
   return id;
