721aaf1271ad3007d2b1bd7e9054bcfc1288d9d9
George Kozirev <gkozirev.0@gmail.com>
2026-07-18 18:34:44 +0300

    comment system.h

--- src/system.h
+++ src/system.h
@@ -9,9 +9,16 @@ class System {
 public:
   virtual ~System() = default;
 
+  /// The System has unlimited access to the `World`,
+  /// it can query any sets of Components, add and remove Entities,
+  /// add and remove Components.
+  /// The behaviour is unstable if the System adds/removes Entities/Components
+  /// while iterating over `View`.
+  /// The System must not call `World::update`.
   virtual void update(World &world, float dt) const = 0;
 };
 
+/// Allocates a system of a given type into a `unique_ptr`.
 template <typename T> std::unique_ptr<System> make_system() {
   return std::make_unique<T>();
 }
